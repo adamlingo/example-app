4a09d4e663e63e98f128251a86a96759ee501cd8
adamlingo <adamlingo@yahoo.com>
2019-09-16 11:23:13 -0500

    Add posts/:id ash PostShowComponent show route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// routes for project listed here
 import { PostListComponent } from './post/post-list.component';
+import { PostShowComponent } from './post/post-show.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	{ path: 'home',  component: HomepageComponent },
 	{ path: 'posts', component: PostListComponent },
+	{ path: 'posts/:id', component: PostShowComponent },
 ]
 
 @NgModule({
